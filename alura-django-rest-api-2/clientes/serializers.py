fb6ce377bba4de9c31ebfc1d001852bd7d25490c
Paulo César <paulosouza90@gmail.com>
2022-01-18 21:32:04 -0300

    implementando validadores

--- alura-django-rest-api-2/clientes/serializers.py
+++ alura-django-rest-api-2/clientes/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validator import *
+
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+
+    def validate(self, data):
+
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({"cpf": "O tamanho do cpf deve ser de 11 digitos."})
+
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({"nome": "Não insira números aqui."})
+
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({"rg": "O rg é formado por 9 dígitos."})
+
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({"celular": "Número inválido. Siga o padrão (xx xxxxx-xxxx)"})
+
+        return data
---
+++ alura-django-rest-api-2/clientes/validator.py
@@ -0,0 +1,19 @@
+import re
+
+
+def cpf_valido(cpf):
+    return len(cpf) == 11
+
+
+def nome_valido(nome):
+    return nome.isalpha()
+
+
+def rg_valido(rg):
+    return len(rg) == 9
+
+
+def celular_valido(celular):
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, celular)
+    return resposta
